bf5f0e85475817dd1fbf59fd0f2991bc158355b6
Ryan-J-W <70917423+Ryan-J-W@users.noreply.github.com>
2020-09-22 23:22:53 -0400

    Update RecordVoice.py
    getting closer, but i keep running into the seg fault errors.  will try a different approach if this persists

--- RecordVoice.py
+++ RecordVoice.py
@@ -1,4 +1,19 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
 import pyaudio
+fs = 44100  # Sample rate
+seconds = 10.5  # Duration of recording
+stuff = []
+recording = pyaudio.PyAudio()
+rec = recording.open(rate = fs, channels=1, format=pyaudio.paInt32, input=True)
+for i in range(0, int(fs / 1024 * 10)):
+    stuff.append(rec.read(1024))
 
 
-audio = pyaudio.PyAudio()
+
+rec.stop_stream()
+rec.close()
+print(stuff)
+'''sd.wait()  # Wait until recording is finished
+write('output.wav', fs, myrecording)  # Save as WAV file
+'''
